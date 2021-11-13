d838316506ffab6621155947c75968642c6bf034
AnkelMauCastillo <88917359+AnkelMauCastillo@users.noreply.github.com>
2021-11-13 02:18:36 -0600

    Final update

--- src/main/java/mx/edu/uacm/progweb/tienda/ClaseImplementadora.java
+++ src/main/java/mx/edu/uacm/progweb/tienda/ClaseImplementadora.java
@@ -3,9 +3,10 @@ package mx.edu.uacm.progweb.tienda;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ClaseImplementadora {
+public class ClaseImplementadora implements Interface{
 
-    public List <String> obtenerListado(int entero){
+    @Override
+	public List <String> obtenerListado(int entero){
         String s = "wewer";
         List<String> lista = new ArrayList<>();
         lista.add(s);
